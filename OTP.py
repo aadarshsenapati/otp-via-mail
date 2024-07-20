e405acff5c6e099c2228b0b122c63feb065c7816
Aadarsh Senapati <57413192+aadarshsenapati@users.noreply.github.com>
2024-07-20 22:55:42 +0530

    Add files via upload

---
+++ OTP.py
@@ -0,0 +1,17 @@
+#yqcn cbbg wdqg egor
+import smtplib
+import maskpass
+smtp_object = smtplib.SMTP('smtp.gmail.com',587)
+smtp_object.ehlo()
+smtp_object.starttls()
+mail=email = input("Enter your email: ")
+password= maskpass.askpass("Enter app password: ")
+print(smtp_object.login(email,password))
+from_e='noreply@gmail.com'
+to_e= input("Enter reciver's mail ID: ")
+subject="OTP"
+import random
+number = random.randint(1111, 9999)
+text="Your OTP is: "
+msg = f"Subject: {subject}\n\n{text}{number}"
+smtp_object.sendmail(from_e,to_e,msg)
